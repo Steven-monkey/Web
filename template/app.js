c0199cf8baf6713e2297853552a787a64bd690bb
Steven-monkey <445928814@qq.com>
2020-04-21 15:59:42 +0800

    每天进步一点点

--- template/app.js
+++
@@ -1,9 +0,0 @@
-//导入模板引擎
-const template = require('art-template')
-const path = require('path')
-const views = path.join(__dirname, 'views', 'index.html')
-const html = template(views, {
-    name: '康彦军',
-    age: 22
-})
-console.log(html);
